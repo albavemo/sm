09b1bc743b66eb76f4036900d0730b497fd65f79
albavemo <alba.vendrellm@e-campus.uab.cat>
2020-05-17 18:41:18 +0200

    Speech to text function

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ function generateAudio(textFile){
 	const textToSpeech = require('@google-cloud/text-to-speech');
 
 	// Import other required libraries
-	//const fs = require('fs');
 	const util = require('util');
 	// Creates a client
 	const client = new textToSpeech.TextToSpeechClient();
@@ -30,7 +29,7 @@ function generateAudio(textFile){
 	  const request = {
 	    input: {text: text},
 	    // Select the language and SSML voice gender (optional)
-	    voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+	    voice: {languageCode: 'es-ES', ssmlGender: 'NEUTRAL'},
 	    // select the type of audio encoding
 	    audioConfig: {audioEncoding: 'MP3'},
 	  };
@@ -39,24 +38,70 @@ function generateAudio(textFile){
 	  const [response] = await client.synthesizeSpeech(request);
 	  // Write the binary audio content to a local file
 	  const writeFile = util.promisify(fs.writeFile);
-	  await writeFile('output.mp3', response.audioContent, 'binary');
-	  console.log('Audio content written to file: output.mp3');
+	  await writeFile('media/t2p_output.mp3', response.audioContent, 'binary');
+	  console.log('Audio content written to file: t2s_output.mp3 \n');
 	}
 	quickStart();
 }
 
 
+function generateText(audioFile){
+	// Imports the Google Cloud client library
+	const speech = require('@google-cloud/speech');
+
+	async function main() {
+		// Creates a client
+		const client = new speech.SpeechClient();
+	  	// The audio to synthesize
+		const audioBytes = audioFile.toString('base64');
+
+		const audio = {
+			content: audioBytes
+		};
+
+		const config = {
+			encoding: 'LINEAR16',
+			//sampleRateHertz: 16000,
+			languageCode: 'en-US'
+			//languageCode: 'es-ES'
+		};
+
+		const request = {
+			audio: audio,
+			config: config
+		};
+
+  		// Detects speech in the audio file
+ 		const [response] = await client.recognize(request);
+		const transcription = response.results.map(result =>
+			result.alternatives[0].transcript).join('\n');
+			console.log('Transcription: ', transcription, '\n');
+
+		// Create output file
+		fs.writeFile("media/s2t_output.txt", transcription, (err) => {
+  		if (err) throw err;
+ 			console.log('Text content written to file: s2t_output.txt \n');
+		});
+	}
+	main().catch(console.error);
+}
 
 //here goes the routing
-console.log("OKI")
 app.get('/', (req, res) => (res.send(index.html)));
 app.get('/crawler', (req,res) => {
 
+	var source = '/Users/AlbaVendrellMoya/Desktop/SM/hackathon/sm-hackovid/media/'
+	var textInput_Name = 'data.txt'
+	var audioInput_Name = 'example3.wav'
 
-	var textFile = fs.readFileSync('/Users/AlbaVendrellMoya/Desktop/SM/hackathon/sm-hackovid/data.txt','utf8');
+	var textFile = fs.readFileSync(source + textInput_Name,'utf8');
+	var audioFile = fs.readFileSync(source +  audioInput_Name);
+
 	generateAudio(textFile);
+	generateText(audioFile);
 	
 });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!` + '\n'));
 
 
